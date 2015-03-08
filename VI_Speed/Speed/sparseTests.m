e8804b0bff0ac2f14f0390edb40538eb915f788c
damiancclarke <damian.c.clarke@gmail.com>
2015-03-08 19:30:33 -0300

    Replacing factor with Factor

--- VI_Speed/Speed/sparseTests.m
+++ VI_Speed/Speed/sparseTests.m
@@ -4,7 +4,7 @@
 % (1) Set up test size, prefill results vector
 %===============================================================================
 N 		= 1000;
-factor		= NaN(N,2);
+Fxactor		= NaN(N,2);
 
 %===============================================================================
 % (2) Test memory improvement of sparse matrix versus non-sparse as N increases
@@ -18,7 +18,7 @@ for i = 1:N
 	mem_c	= whos('cool');
 	mem_c 	= mem_c.bytes;
 
-	factor(i,1) = mem_n/mem_c;
+	Factor(i,1) = mem_n/mem_c;
 	clear naive cool
 end
 
@@ -32,7 +32,7 @@ for i = 1:N
         tic; inv(naive); n=toc;
         tic; inv(cool);  c=toc;
 
-        factor(i,2) = n/c;
+        Factor(i,2) = n/c;
         clear naive cool
 end
 
@@ -40,13 +40,13 @@ end
 % (4) Graphical output
 %===============================================================================
 subplot(2,1,1)
-plot(1:N,factor(:,1), 'r', 'LineWidth', 2)
+plot(1:N,Factor(:,1), 'r', 'LineWidth', 2)
 xlabel('Size of Matrix (N\times N)', 'FontSize', 10)
 ylabel('Proportional Saving in Memory', 'FontSize', 10)
 title('The Advantage of Using Sparse Commands As N\rightarrow 1000',...
 			'FontSize', 14)
 
 subplot(2,1,2)
-scatter(1:N,factor(:,2), 'LineWidth', 2)
+scatter(1:N,Factor(:,2), 'LineWidth', 2)
 xlabel('Inverse of Matrix (N\times N)', 'FontSize', 10)
 ylabel('Proportional Saving in Time', 'FontSize', 10)
